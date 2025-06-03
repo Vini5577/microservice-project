5fe14bab9e1b4c0a4fe3a1bd04c4a5f317dd0b45
Hugo Vinícius Pessoa de Melo Santos <hugo.santos@urbana-pe.com.br>
2025-06-03 10:02:45 -0300

    feat: add jakarta bean validation

---
+++ hd-commons-lib/src/main/java/models/exceptions/ValidationException.java
@@ -0,0 +1,27 @@
+package models.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+import java.util.List;
+import java.util.logging.Filter;
+
+
+@SuperBuilder
+public class ValidationException extends StandardError{
+
+    @Getter
+    private List<FieldError> errors;
+
+    @Getter
+    @AllArgsConstructor
+    private static class FieldError {
+        private String fielName;
+        private String message;
+    }
+
+    public void addError(final String fieldName, final String message) {
+        this.errors.add(new FieldError(fieldName, message));
+    }
+}
