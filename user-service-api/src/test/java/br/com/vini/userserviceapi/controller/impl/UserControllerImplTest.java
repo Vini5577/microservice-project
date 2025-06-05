84a5346b884677e75c1ed65aea1e8ffaa1116bc2
Vini5577 <ugopessoa@gmail.com>
2025-06-05 19:17:12 -0300

    test: add integration test to find all user controller with success

--- user-service-api/src/test/java/br/com/vini/userserviceapi/controller/impl/UserControllerImplTest.java
+++ user-service-api/src/test/java/br/com/vini/userserviceapi/controller/impl/UserControllerImplTest.java
@@ -11,6 +11,8 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.List;
+
 import static br.com.vini.userserviceapi.creator.CreatorUtils.generateMock;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -53,5 +55,23 @@ class UserControllerImplTest {
                 .andExpect(jsonPath("$.path").value("/api/users/123"))
                 .andExpect(jsonPath("$.status").value(NOT_FOUND.value()))
                 .andExpect(jsonPath("$.timestamp").isNotEmpty());
+    }
+
+    @Test
+    void testFindAllWithSuccess() throws Exception {
+        final var entity1 = generateMock(User.class);
+        final var entity2 = generateMock(User.class);
+
+        userRepository.saveAll(List.of(entity1, entity2));
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0]").isNotEmpty())
+                .andExpect(jsonPath("$[1]").isNotEmpty())
+                .andExpect(jsonPath("$[0].profiles").isArray())
+                .andExpect(jsonPath("$[1].profiles").isArray());
+
+        userRepository.deleteAll(List.of(entity1, entity2));
+
     }
 }
