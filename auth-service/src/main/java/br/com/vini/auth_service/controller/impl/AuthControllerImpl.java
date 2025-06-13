c7cbd1d99940047f42990052c125073460600ce6
Vini5577 <ugopessoa@gmail.com>
2025-06-13 19:12:34 -0300

    feat: add authentication impl

--- auth-service/src/main/java/br/com/vini/auth_service/controller/impl/AuthControllerImpl.java
+++ auth-service/src/main/java/br/com/vini/auth_service/controller/impl/AuthControllerImpl.java
@@ -1,18 +1,25 @@
 package br.com.vini.auth_service.controller.impl;
 
 import br.com.vini.auth_service.controller.AuthController;
+import br.com.vini.auth_service.security.dtos.JWTAuthenticationImpl;
+import br.com.vini.auth_service.utils.JWTUtils;
+import lombok.RequiredArgsConstructor;
 import models.requests.AuthenticateRequest;
 import models.responses.AuthenticationResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class AuthControllerImpl implements AuthController {
+
+    private final JWTUtils jwtUtils;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
     @Override
-    public ResponseEntity<AuthenticationResponse> authenticate(AuthenticateRequest request) {
-        return ResponseEntity.ok().body(AuthenticationResponse.builder()
-                        .type("Bearer")
-                        .token("token")
-                .build());
+    public ResponseEntity<AuthenticationResponse> authenticate(final AuthenticateRequest request) throws Exception {
+        return ResponseEntity.ok().body(
+                new JWTAuthenticationImpl(jwtUtils, authenticationConfiguration.getAuthenticationManager()).authenticate(request));
     }
 }
