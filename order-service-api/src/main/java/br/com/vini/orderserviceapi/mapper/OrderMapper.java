15b264d84baf02e7c0d6a14f09843187e3a9a4bf
Vini5577 <ugopessoa@gmail.com>
2025-10-29 19:43:31 -0300

    fix: bugfix on create new order

--- order-service-api/src/main/java/br/com/vini/orderserviceapi/mapper/OrderMapper.java
+++ order-service-api/src/main/java/br/com/vini/orderserviceapi/mapper/OrderMapper.java
@@ -6,7 +6,10 @@ import models.requests.CreateOrderRequest;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Named;
+import org.springframework.cglib.core.Local;
 
+
+import java.time.LocalDateTime;
 
 import static org.mapstruct.NullValueCheckStrategy.ALWAYS;
 import static org.mapstruct.NullValuePropertyMappingStrategy.IGNORE;
@@ -20,10 +23,15 @@ public interface OrderMapper {
 
     @Mapping(target = "id", ignore = true)
     @Mapping(target = "status", source = "status", qualifiedByName = "mapStatus")
+    @Mapping(target = "createdAt", expression = "java(mapCreatedAt())")
     Order fromRequest(CreateOrderRequest createOrderRequest);
 
     @Named("mapStatus")
     default OrderStatusEnum mapStatus(final String status) {
         return OrderStatusEnum.toEnum(status);
+    }
+
+    default LocalDateTime mapCreatedAt() {
+        return LocalDateTime.now();
     }
 }
